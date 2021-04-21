3fc74c4987c95b29f2764948ccfdf6fdd874bbcc
Souheil Chelfouh <trollfot@gmail.com>
2021-04-21 19:18:10 +0200

    TokenException no longer take the token as required arguments

--- src/roughrider/token/errors.py
+++ src/roughrider/token/errors.py
@@ -1,16 +1,12 @@
 class TokenException(Exception):
-
-    def __init__(self, token: str):
-        self.token = token
+    pass
 
 
 class InvalidToken(TokenException):
-
-    def __str__(self):
-        return f"Token {self.token!r} could not be parsed."
+    """Token could not be parsed.
+    """
 
 
 class ExpiredToken(TokenException):
-
-    def __str__(self):
-        return f"Token {self.token!r} is expired."
+    """Token is expired.
+    """
